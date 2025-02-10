76ad424aaa7c0f5e332975aec23d73ce42fa7308
Gustavo Comerlato <gustavocomerlato@gmail.com>
2025-02-10 16:36:30 -0300

    Fixed bug when handling multiple sensors

--- movella_dot/include/movella_dot.hpp
+++ movella_dot/include/movella_dot.hpp
@@ -14,6 +14,7 @@
 #include "movella_msgs/msg/dot_sensor.hpp"
 #include <sensor_msgs/msg/imu.hpp>
 #include <sensor_msgs/msg/magnetic_field.hpp>
+#include <unordered_map>
 // #include <geometry_msgs/msg/quaternion.hpp>
 
 namespace movella_dot
@@ -59,23 +60,18 @@ public:
 	};
 
 private:
-	int config;	 // Sensor configuration
-
 	// ROS
 	rclcpp::TimerBase::SharedPtr timer_;
 	double t0_;
 	std::chrono::milliseconds xdpc_period_;	 // xdpc callback period (ms)
+	std::unordered_map<int,XsDotDevicePtr> sensor_table_;
 
 	std::vector<rclcpp::Publisher<movella_msgs::msg::DotSensor>::SharedPtr> dotPub_;	 // DOT Message publisher
 	std::vector<rclcpp::Publisher<sensor_msgs::msg::Imu>::SharedPtr> imuPub_;					 // IMU(Quat,Gyro,Acc) measurements
 	std::vector<rclcpp::Publisher<sensor_msgs::msg::MagneticField>::SharedPtr> magPub_;	// Magnetic Field components
 
-	std::vector<movella_msgs::msg::DotSensor> dotMsgs_;		// DOT Messages
-	std::vector<sensor_msgs::msg::Imu> imuMsgs_;						// IMU Messages
-	std::vector<sensor_msgs::msg::MagneticField> magMsgs_;	// Magnetic Field Messages
-
-	void timer_callback();	// Reads XSens DOT via handler and prepares messages for publishers, at an interval of
-													// xdpc_period ms
+	void timer_callback();	// Triggers packet_callback for each DOT, at an interval of xdpc_period ms
+	void packet_callback(int device_num); // XSens DOT Callback: Reads packet and publishes it
 };
 }	 // namespace movella_dot
 #endif
--- movella_dot/src/movella_dot.cpp
+++ movella_dot/src/movella_dot.cpp
@@ -21,12 +21,9 @@ namespace movella_dot
 			RCLCPP_ERROR_STREAM(get_logger(), "Error: Failed to initialize XDPC Handler component.");
 			return;
 		}
-		xdpc_period_ = 100ms;
+		xdpc_period_ = 10ms;
 
 		// ROS 2 Publishers setup
-
-		timer_ = rclcpp::create_timer(this, this->get_clock(), xdpc_period_, std::bind(&MovellaDot::timer_callback, this));
-
 		rclcpp::PublisherOptions pub_options;
 		pub_options.qos_overriding_options =
 				rclcpp::QosOverridingOptions::with_default_policies();	// Allow overriding QoS settings (history, depth,
@@ -35,21 +32,10 @@ namespace movella_dot
 		int numdots = 0;
 		for (auto& device : xdpcHandler_.connectedDots())
 		{
-			// Create IMU publisher and append to list
-			std::string topic = "/dot_"s + std::to_string(numdots + 1) + "/IMU"s;
-			auto pubI = create_publisher<sensor_msgs::msg::Imu>(topic, rclcpp::QoS(100), pub_options);
-			imuPub_.push_back(pubI);
-
-			// Create magnetic field publisher and append to list
-			topic = "/dot_"s + std::to_string(numdots + 1) + "/magnetic_field"s;
-			auto pubM = create_publisher<sensor_msgs::msg::MagneticField>(topic, rclcpp::QoS(100), pub_options);
-			magPub_.push_back(pubM);
-
-			// Create DOT data publisher and append to list
-			topic = "/dot_"s + std::to_string(numdots + 1) + "/dot"s;
-			auto pubD = create_publisher<movella_msgs::msg::DotSensor>(topic, rclcpp::QoS(100), pub_options);
-			dotPub_.push_back(pubD);
-
+			if (!device->setOutputRate(60))
+				RCLCPP_INFO_STREAM(get_logger(),"Failed to set 60 Hz rate"); // taxa de publicação
+			else
+			RCLCPP_INFO_STREAM(get_logger(),"Output rate: " << std::to_string(device->outputRate())); // taxa de publicação
 			// Set device filter and payload profiles
 			auto filterProfiles = device->getAvailableFilterProfiles();
 			RCLCPP_INFO_STREAM(get_logger(), std::to_string(filterProfiles.size()) + " available filter profiles: ");
@@ -76,12 +62,29 @@ namespace movella_dot
 				continue;
 			}
 
+			// Create IMU publisher and append to list
+			std::string topic = "/dot_"s + std::to_string(numdots + 1) + "/IMU"s;
+			auto pubI = create_publisher<sensor_msgs::msg::Imu>(topic, rclcpp::QoS(100), pub_options);
+			imuPub_.push_back(pubI);
+
+			// Create magnetic field publisher and append to list
+			topic = "/dot_"s + std::to_string(numdots + 1) + "/magnetic_field"s;
+			auto pubM = create_publisher<sensor_msgs::msg::MagneticField>(topic, rclcpp::QoS(100), pub_options);
+			magPub_.push_back(pubM);
+
+			// Create DOT data publisher and append to list
+			topic = "/dot_"s + std::to_string(numdots + 1) + "/dot"s;
+			auto pubD = create_publisher<movella_msgs::msg::DotSensor>(topic, rclcpp::QoS(100), pub_options);
+			dotPub_.push_back(pubD);
+
+
+			device->setDeviceTagName("XSens DOT "s + std::to_string(numdots+1));
+			this->sensor_table_[numdots] = device;
 			numdots++;	// increase iterator
 		}
 		RCLCPP_INFO_STREAM(get_logger(), "Found " << numdots << " DOT sensors!");
-		dotMsgs_.reserve(numdots);
-		imuMsgs_.reserve(numdots);
-		magMsgs_.reserve(numdots);
+
+		timer_ = rclcpp::create_timer(this, this->get_clock(), xdpc_period_, std::bind(&MovellaDot::timer_callback, this));
 		// RCLCPP_INFO_STREAM(get_logger(), "Main loop. Publishing data to ROS 2 topics");
 
 		// First printing some headers so we see which data belongs to which device
@@ -121,147 +124,144 @@ namespace movella_dot
 	///
 	void MovellaDot::timer_callback(void)
 	{
-		RCLCPP_INFO_STREAM(this->get_logger(), "Polling DOT sensors for publishing...");
+		RCLCPP_DEBUG_STREAM(this->get_logger(), "Polling DOT sensors for publishing...");
 
+		for (long unsigned int dd=0;dd<xdpcHandler_.connectedDots().size(); dd++){
+			RCLCPP_DEBUG_STREAM(get_logger(),"Device "<<std::to_string(dd) << " status: "
+				 << std::to_string(xdpcHandler_.packetAvailable(sensor_table_[dd]->bluetoothAddress())));
+		}
 		if (xdpcHandler_.packetsAvailable())
 		{
-			RCLCPP_INFO_STREAM(this->get_logger(), "Available packets detected!");
-			// cout << "\r";
-			int device_num = -1;
-			for (auto const& device : xdpcHandler_.connectedDots())
-			{
-				device_num++;
-				// Retrieve a packet from the device
+			RCLCPP_DEBUG_STREAM(this->get_logger(), "All packets available. Publishing...");
 
-				RCLCPP_INFO_STREAM(this->get_logger(), "Getting packet from device " << device_num);
-				XsDataPacket packet = xdpcHandler_.getNextPacket(device->bluetoothAddress());
-
-
-				// RCLCPP_INFO_STREAM(this->get_logger(), "Setting segment and seglist");
-				// std::string segment, seglist;
-				// std::stringstream test(device->bluetoothAddress().c_str());
-				// while (std::getline(test, segment, ':'))
-				// {
-				// 	seglist = segment;
-				// }
-
-
-				if (packet.containsStatus())	// Filling in (legacy?) status
-				{
-					RCLCPP_INFO_STREAM(this->get_logger(), "Got status!");
-					uint32_t st = packet.status();
-
-					dotMsgs_[device_num].status = st;
-				}
-				if (packet.containsSampleTimeFine())	// Sample time fine
-				{
-
-					RCLCPP_INFO_STREAM(this->get_logger(), "Got sampleTimeFine!");
-					uint32_t stf = packet.sampleTimeFine();
-
-					dotMsgs_[device_num].sample_time_fine = stf;
-				}
-				if (packet.containsVelocityIncrement())	 // Sample time fine
-				{
-					RCLCPP_INFO_STREAM(this->get_logger(), "Got velocityIncrement!");
-					XsVector dv = packet.velocityIncrement();
-
-					dotMsgs_[device_num].delta_velocity.x = dv[0];
-					dotMsgs_[device_num].delta_velocity.y = dv[1];
-					dotMsgs_[device_num].delta_velocity.z = dv[2];
-				}
-				if (packet.containsOrientation())	 // Filling in orientation data
-				{
-					RCLCPP_INFO_STREAM(this->get_logger(), "Got quaternion!");
-					XsQuaternion quat = packet.orientationQuaternion();
-
-					// Fills in quaternion data
-					dotMsgs_[device_num].quaternion.x = quat[0];
-					dotMsgs_[device_num].quaternion.y = quat[1];
-					dotMsgs_[device_num].quaternion.z = quat[2];
-					dotMsgs_[device_num].quaternion.w = quat[3];	 // Real part
-
-					imuMsgs_[device_num].orientation.x = quat[0];
-					imuMsgs_[device_num].orientation.y = quat[1];
-					imuMsgs_[device_num].orientation.z = quat[2];
-					imuMsgs_[device_num].orientation.w = quat[3];	 // Real part
-				}
-				if (packet.containsOrientationIncrement())	// Quaternion rate
-				{
-					RCLCPP_INFO_STREAM(this->get_logger(), "Got orientationIncrement!");
-					XsQuaternion dq = packet.orientationIncrement();
-
-					// Fills in quaternion data
-					dotMsgs_[device_num].delta_quaternion.x = dq[0];
-					dotMsgs_[device_num].delta_quaternion.y = dq[1];
-					dotMsgs_[device_num].delta_quaternion.z = dq[2];
-					dotMsgs_[device_num].delta_quaternion.w = dq[3];	 // Real part
-				}
-				if (packet.containsCalibratedAcceleration())	 // Filling in acceleration data
-				{
-					RCLCPP_INFO_STREAM(this->get_logger(), "Got calibratedAcceleration!");
-					XsVector acc = packet.calibratedAcceleration();
-
-					imuMsgs_[device_num].linear_acceleration.x = acc[0];
-					imuMsgs_[device_num].linear_acceleration.y = acc[1];
-					imuMsgs_[device_num].linear_acceleration.z = acc[2];
-					
-					dotMsgs_[device_num].linear_acceleration.x = acc[0];
-					dotMsgs_[device_num].linear_acceleration.y = acc[1];
-					dotMsgs_[device_num].linear_acceleration.z = acc[2];
-				}
-				if (packet.containsCalibratedGyroscopeData())	// Filling in gyrocope data
-				{
-					// Note: there is a rawGyroscopeTemperatureData, which _also_ returns a vector.
-					// I'm not sure how the temperature is used with that call; does it contain compensated gyro signals or not?
-
-					RCLCPP_INFO_STREAM(this->get_logger(), "Got calibratedGyroscopeData!");
-					XsVector gyro = packet.calibratedGyroscopeData();
-
-					imuMsgs_[device_num].angular_velocity.x = gyro[0];
-					imuMsgs_[device_num].angular_velocity.y = gyro[1];
-					imuMsgs_[device_num].angular_velocity.z = gyro[2];
-
-					dotMsgs_[device_num].angular_velocity.x = gyro[0];
-					dotMsgs_[device_num].angular_velocity.y = gyro[1];
-					dotMsgs_[device_num].angular_velocity.z = gyro[2];
-				}
-				if (packet.containsCalibratedMagneticField())	// Filling in magnetometer data
-				{
-					RCLCPP_INFO_STREAM(this->get_logger(), "Got calibratedMagneticField!");
-					XsVector mag = packet.calibratedMagneticField();
-
-					magMsgs_[device_num].magnetic_field.x = mag[0];
-					magMsgs_[device_num].magnetic_field.y = mag[1];
-					magMsgs_[device_num].magnetic_field.z = mag[2];
-
-					dotMsgs_[device_num].magnetic_field.x = mag[0];
-					dotMsgs_[device_num].magnetic_field.y = mag[1];
-					dotMsgs_[device_num].magnetic_field.z = mag[2];
-				}
-
-				// Triggers publishers
-				RCLCPP_INFO_STREAM(this->get_logger(), "Preparing imu message...");
-				imuMsgs_[device_num].header.stamp = this->get_clock()->now();
-				RCLCPP_INFO_STREAM(this->get_logger(), "Getting device bluetooth ID...");
-				imuMsgs_[device_num].header.frame_id = device->bluetoothAddress().toStdString();
-				RCLCPP_INFO_STREAM(this->get_logger(), "Publishing IMU message...");
-				imuPub_[device_num]->publish(imuMsgs_[device_num]);
-
-
-				RCLCPP_INFO_STREAM(this->get_logger(), "Publishing magnetic field message...");
-				magMsgs_[device_num].header.stamp = this->get_clock()->now();
-				magMsgs_[device_num].header.frame_id = device->bluetoothAddress().toStdString();
-				magPub_[device_num]->publish(magMsgs_[device_num]);
-
-				RCLCPP_INFO_STREAM(this->get_logger(), "Publishing dotSensor message...");
-				dotMsgs_[device_num].header.stamp = this->get_clock()->now();
-				dotMsgs_[device_num].header.frame_id = device->bluetoothAddress().toStdString();
-				dotPub_[device_num]->publish(dotMsgs_[device_num]);
+			for (long unsigned int dd=0;dd<xdpcHandler_.connectedDots().size(); dd++){
+				packet_callback(dd);
 			}
-			// cout << flush;
+			XsTime::msleep(0);	
+		}	
+	};
+
+	void MovellaDot::packet_callback(int device_num)
+	{
+		XsDotDevicePtr device = sensor_table_[device_num];
+		RCLCPP_DEBUG_STREAM(this->get_logger(), "Getting packet from device " << device->deviceTagName());
+		XsDataPacket packet = xdpcHandler_.getNextPacket(device->bluetoothAddress());
+		// Retrieve a packet from the device and prepares message
+		movella_msgs::msg::DotSensor dotMsg;		// DOT Messages
+		sensor_msgs::msg::Imu imuMsg;						// IMU Messages
+		sensor_msgs::msg::MagneticField magMsg;	// Magnetic Field Messages
+
+		if (packet.containsStatus())	// Filling in (legacy?) status
+		{
+			RCLCPP_DEBUG_STREAM(this->get_logger(), "Got status!");
+			uint32_t st = packet.status();
+
+			dotMsg.status = st;
 		}
-		XsTime::msleep(0);
+		if (packet.containsSampleTimeFine())	// Sample time fine
+		{
+			RCLCPP_DEBUG_STREAM(this->get_logger(), "Got sampleTimeFine!");
+			uint32_t stf = packet.sampleTimeFine();
+
+			dotMsg.sample_time_fine = stf;
+		}
+		if (packet.containsVelocityIncrement())	 // Sample time fine
+		{
+			RCLCPP_DEBUG_STREAM(this->get_logger(), "Got velocityIncrement!");
+			XsVector dv = packet.velocityIncrement();
+
+			dotMsg.delta_velocity.x = dv[0];
+			dotMsg.delta_velocity.y = dv[1];
+			dotMsg.delta_velocity.z = dv[2];
+		}
+		if (packet.containsOrientation())	 // Filling in orientation data
+		{
+			RCLCPP_DEBUG_STREAM(this->get_logger(), "Got quaternion!");
+			XsQuaternion quat = packet.orientationQuaternion();
+
+			// Fills in quaternion data
+			dotMsg.quaternion.x = quat[0];
+			dotMsg.quaternion.y = quat[1];
+			dotMsg.quaternion.z = quat[2];
+			dotMsg.quaternion.w = quat[3];	 // Real part
+
+			imuMsg.orientation.x = quat[0];
+			imuMsg.orientation.y = quat[1];
+			imuMsg.orientation.z = quat[2];
+			imuMsg.orientation.w = quat[3];	 // Real part
+		}
+		if (packet.containsOrientationIncrement())	// Quaternion rate
+		{
+			RCLCPP_DEBUG_STREAM(this->get_logger(), "Got orientationIncrement!");
+			XsQuaternion dq = packet.orientationIncrement();
+
+			// Fills in quaternion rate data
+			dotMsg.delta_quaternion.x = dq[0];
+			dotMsg.delta_quaternion.y = dq[1];
+			dotMsg.delta_quaternion.z = dq[2];
+			dotMsg.delta_quaternion.w = dq[3];	 // Real part
+		}
+		if (packet.containsCalibratedAcceleration())	 // Filling in acceleration data
+		{
+			RCLCPP_DEBUG_STREAM(this->get_logger(), "Got calibratedAcceleration!");
+			XsVector acc = packet.calibratedAcceleration();
+
+			imuMsg.linear_acceleration.x = acc[0];
+			imuMsg.linear_acceleration.y = acc[1];
+			imuMsg.linear_acceleration.z = acc[2];
+			
+			dotMsg.linear_acceleration.x = acc[0];
+			dotMsg.linear_acceleration.y = acc[1];
+			dotMsg.linear_acceleration.z = acc[2];
+		}
+		if (packet.containsCalibratedGyroscopeData())	// Filling in gyrocope data
+		{
+			// Note: there is a rawGyroscopeTemperatureData, which _also_ returns a vector.
+			// I'm not sure how the temperature is used with that call; does it contain compensated gyro signals or not?
+
+			RCLCPP_DEBUG_STREAM(this->get_logger(), "Got calibratedGyroscopeData!");
+			XsVector gyro = packet.calibratedGyroscopeData();
+
+			imuMsg.angular_velocity.x = gyro[0];
+			imuMsg.angular_velocity.y = gyro[1];
+			imuMsg.angular_velocity.z = gyro[2];
+
+			dotMsg.angular_velocity.x = gyro[0];
+			dotMsg.angular_velocity.y = gyro[1];
+			dotMsg.angular_velocity.z = gyro[2];
+		}
+		if (packet.containsCalibratedMagneticField())	// Filling in magnetometer data
+		{
+			RCLCPP_DEBUG_STREAM(this->get_logger(), "Got calibratedMagneticField!");
+			XsVector mag = packet.calibratedMagneticField();
+
+			magMsg.magnetic_field.x = mag[0];
+			magMsg.magnetic_field.y = mag[1];
+			magMsg.magnetic_field.z = mag[2];
+
+			dotMsg.magnetic_field.x = mag[0];
+			dotMsg.magnetic_field.y = mag[1];
+			dotMsg.magnetic_field.z = mag[2];
+		}
+
+		// Prepares message
+		RCLCPP_DEBUG_STREAM(this->get_logger(), "Preparing imu message: ");
+		imuMsg.header.stamp = this->get_clock()->now();
+		imuMsg.header.frame_id = device->deviceTagName().toStdString();
+		RCLCPP_DEBUG_STREAM(this->get_logger(), "Publishing IMU message...");
+
+		RCLCPP_DEBUG_STREAM(this->get_logger(), "Publishing magnetic field message...");
+		magMsg.header.stamp = this->get_clock()->now();
+		magMsg.header.frame_id = device->deviceTagName().toStdString() ;
+
+		RCLCPP_DEBUG_STREAM(this->get_logger(), "Publishing dotSensor message...");
+		dotMsg.header.stamp = this->get_clock()->now();
+		dotMsg.header.frame_id = device->deviceTagName().toStdString();
+
+		imuPub_[device_num]->publish(imuMsg);
+		magPub_[device_num]->publish(magMsg);	
+		dotPub_[device_num]->publish(dotMsg);
+
 	};
 }	 // namespace movella_dot
 
